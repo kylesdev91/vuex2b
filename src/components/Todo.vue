0ba607c93e975fec3f430245aaf7058b97d8fa42
Kyle Hernandez <kylesdev91@yahoo.com>
2022-11-27 10:09:13 -0600

    basic todo app anda bout to add styles

---
+++ src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ todo.title }}
+    <button @click="deleteTodo">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["todo"],
+
+  methods: {
+    deleteTodo() {
+      this.$store.dispatch("deleteTodo", this.todo);
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/store.js
+++ src/store.js
@@ -4,16 +4,37 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-
+  state: {
+    todos: [
+      {
+        title: "todo item a",
+        completed: false,
+      },
+      {
+        title: "todo item b",
+        completed: false,
+      },
+    ],
+  },
+  getters: {},
+  mutations: {
+    NEW_TODO(state, todoItem) {
+      state.todos.push({
+        title: todoItem,
+        completed: false,
+      });
     },
-    getters: {
-
+    DELETE_TODO(state, todoItem) {
+      let index = state.todos.indexOf(todoItem);
+      state.todos.splice(index, 1);
     },
-    mutations: {
-
+  },
+  actions: {
+    addNewTodo({ commit }, todoItem) {
+      commit("NEW_TODO", todoItem);
     },
-    actions: {
-        
-    }
-})
+    deleteTodo({ commit }, todoItem) {
+      commit("DELETE_TODO", todoItem);
+    },
+  },
+});
